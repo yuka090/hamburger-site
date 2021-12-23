1ab5e3f4cb45d7cca82e61577a6055cdb349a4ba
yuka090 <applebutter.myufo@icloud.com>
2021-12-23 21:53:11 +0900

    window追加

--- js/jquery.js
+++ js/jquery.js
@@ -2,23 +2,30 @@ $(document).ready(function() {
 
     jQuery(function($){
         $('.c-header__menu').on('click',function(){
-            $('.l-aside').toggleClass('p-open');
+            $('.c-aside').toggleClass('c-open');
             $('.c-slideback').fadeIn();
             //alert('クリック');
         });
 
         $('.c-aside__close').on('click',function(){
-            $('.l-aside').removeClass('p-open');
+            $('.c-aside').removeClass('c-open');
             $('.c-slideback').fadeOut();
         });
 
         $('.c-slideback').on('click',function(){
-            $('.l-aside').removeClass('p-open');
+            $('.c-aside').removeClass('c-open');
             $('.c-slideback').fadeOut();
         });    
 
     });
     
+    $(window).resize(function() {
+        if (window.matchMedia('(min-width: 1040px)').matches) {
+            //画面横幅が1040px以上のとき
+            $('.c-aside').removeClass('c-open');
+            $('.c-slideback').fadeOut();
+        };
+    });
 
 
 
